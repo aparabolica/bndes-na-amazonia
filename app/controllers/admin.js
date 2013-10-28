a7fffce795c5abec3be91b8cb4a2e31390784fe5
Vitor George <vitor.george@gmail.com>
2013-10-27 22:01:19 -0200

    Load data from csv

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -5,7 +5,11 @@
 
 var mongoose = require('mongoose')
   , Activity = mongoose.model('Activity')
+  , Financing = mongoose.model('Financing')
+  , Project = mongoose.model('Project')
+  , Organization = mongoose.model('Organization')    
   , States = mongoose.model('States')
+  , csv      = require('csv')
   , utils = require('../../lib/utils')
   
   
@@ -31,11 +35,52 @@ exports.index = function(req, res){
  */
 
 exports.populate = function(req,res) {
-  Activity.loadCSV(function(err){
-  	if (err) req.flash('error', 'Erro ao carregar setores de atividade econômica')
-  	States.loadCSV(function(err){
-  		if (err) req.flash('error', 'Erro ao carregar estados')
-      res.redirect('admin')
+  // Remove current data
+  Financing.collection.remove(function(err){
+    if (err) res.render(500) 
+    Project.collection.remove(function(err){
+      if (err) res.render(500) 
+      Organization.collection.remove(function(err){
+        if (err) res.render(500)
+        // starts reading csv
+        csv()
+        .from.path(__dirname+'/../../data/financings.csv', { columns: true, delimiter: ',', escape: '"' })
+        .on('record', function(row,index){
+          beneficiaryName = row['Beneficiado (Empresa/Estado)'].trim()
+          // insert or update beneficiary
+          Organization.findOneAndUpdate({name: beneficiaryName},{$set: { name: beneficiaryName }}, {upsert: true}, function(err,org){
+            if (err) res.render(500)
+            projectInfo = {
+              title: row['Resumo do Projeto'].trim(),
+              description: row['Descrição do Projeto'].trim()
+            }
+            // insert or update project
+            Project.findOneAndUpdate(projectInfo,{$set:projectInfo},{upsert:true},function(err,proj){
+              if (err) res.render(500)
+              financingInfo = {
+                contractDate: moment(row['Contratação'].trim(), 'DD/MM/YYYY'),
+                isDirect: row['Tipo de operação'].trim() == 'direto',
+                project: proj,
+                beneficiary: org,  
+                amount: row['Valor da Operação'].trim()
+              }
+              Financing.findOneAndUpdate(financingInfo,{$set:financingInfo},{upsert:true},function(err){
+                if (err) res.render(500)
+              })
+            })
+          })
+          
+                    
+        })
+        .on('end', function(){
+          Project.updateRelatedFinancings()
+          Organization.updateRelatedFinancings()
+        })
+        .on('error', function(error){
+          res.render(500)
+        })
+        res.redirect('admin')        
+      })
     })
   })
 }
