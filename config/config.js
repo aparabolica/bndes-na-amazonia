ebe7d4f892e7b92abcd6c27f59aaeb0451d6e377
Vitor George <vitor.george@gmail.com>
2013-11-11 02:19:12 -0200

    Configuration file cleaned

--- config/config.js
+++ config/config.js
@@ -1,94 +1,21 @@
 
 var path = require('path')
   , rootPath = path.normalize(__dirname + '/..')
-  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
-  , notifier = {
-      service: 'postmark',
-      APN: false,
-      email: false, // true
-      actions: ['comment'],
-      tplPath: templatePath,
-      key: 'POSTMARK_KEY',
-      parseAppId: 'PARSE_APP_ID',
-      parseApiKey: 'PARSE_MASTER_KEY'
-    }
 
 module.exports = {
   development: {
-    db: process.env.MONGOLAB_URI ||
-      process.env.MONGOHQ_URL || 'mongodb://localhost/elos_dev',
+    db: 'mongodb://localhost/bna_dev',
     root: rootPath,
-    notifier: notifier,
     app: {
-      name: 'Elos'
-    },
-    facebook: {
-      clientID: "APP_ID",
-      clientSecret: "APP_SECRET",
-      callbackURL: "http://localhost:3000/auth/facebook/callback"
-    },
-    twitter: {
-      clientID: "CONSUMER_KEY",
-      clientSecret: "CONSUMER_SECRET",
-      callbackURL: "http://localhost:3000/auth/twitter/callback"
-    },
-    github: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/auth/github/callback'
-    },
-    google: {
-      clientID: "APP_ID",
-      clientSecret: "APP_SECRET",
-      callbackURL: "http://localhost:3000/auth/google/callback"
-    },
-    linkedin: {
-      clientID: "CONSUMER_KEY",
-      clientSecret: "CONSUMER_SECRET",
-      callbackURL: "http://localhost:3000/auth/linkedin/callback"
+      name: 'BNDES na Amazônia'
     }
   },
   test: {
-    db: process.env.MONGOLAB_URI ||
-      process.env.MONGOHQ_URL || 'mongodb://localhost/elos_test',
+    db: 'mongodb://localhost/bna_test',
     root: rootPath,
-    notifier: notifier,
     app: {
-      name: 'Elos'
-    },
-    facebook: {
-      clientID: "APP_ID",
-      clientSecret: "APP_SECRET",
-      callbackURL: "http://localhost:3000/auth/facebook/callback"
-    },
-    twitter: {
-      clientID: "CONSUMER_KEY",
-      clientSecret: "CONSUMER_SECRET",
-      callbackURL: "http://localhost:3000/auth/twitter/callback"
-    },
-    github: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/auth/github/callback'
-    },
-    google: {
-      clientID: "APP_ID",
-      clientSecret: "APP_SECRET",
-      callbackURL: "http://localhost:3000/auth/google/callback"
-    },
-    linkedin: {
-      clientID: "CONSUMER_KEY",
-      clientSecret: "CONSUMER_SECRET",
-      callbackURL: "http://localhost:3000/auth/linkedin/callback"
+      name: 'BNDES na Amazônia'
     }
   },
-  production: {
-    db: process.env.MONGOLAB_URI ||
-      process.env.MONGOHQ_URL || 'mongodb://localhost/heroku_app18624412',
-    root: rootPath,
-    notifier: notifier,
-    app: {
-      name: 'Elos'
-    }
-  }
+  production: {}
 }
